33d517e86a7bd17c40ecbf99c06ab5f1b25d0e66
Florent Fortat <florent@fortat.fr>
2021-10-02 19:13:32 +0200

    Add item pile base script, affected by the wind blow

---
+++ Assets/Scripts/ItemsPile.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+[RequireComponent(typeof(WindController))]
+[RequireComponent(typeof(CharacterController))]
+public class ItemsPile : MonoBehaviour
+{
+    [SerializeField]
+    private float blowPercentageOnPile = 5f;
+
+    private Vector3 pileNormal = Vector3.up;
+
+    private CharacterController characterController = null;
+    private WindController windController = null;
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+        windController = GetComponent<WindController>();
+    }
+
+    private void FixedUpdate()
+    {
+        pileNormal = (pileNormal + windController.WindBlow * blowPercentageOnPile / 100f).normalized;
+        ForDebug(transform.position, pileNormal * 3, Color.red);
+    }
+    public void ForDebug(Vector3 pos, Vector3 direction, Color color, float arrowHeadLength = 0.25f, float arrowHeadAngle = 20.0f)
+    {
+        Debug.DrawRay(pos, direction, color);
+
+        Vector3 right = Quaternion.LookRotation(direction) * Quaternion.Euler(0, 180 + arrowHeadAngle, 0) * new Vector3(0, 0, 1);
+        Vector3 left = Quaternion.LookRotation(direction) * Quaternion.Euler(0, 180 - arrowHeadAngle, 0) * new Vector3(0, 0, 1);
+        Debug.DrawRay(pos + direction, right * arrowHeadLength, color);
+        Debug.DrawRay(pos + direction, left * arrowHeadLength, color);
+    }
+}
